aba006e3bea76bf7af9e9dc56603c4c349964571
aszhl <139355448+aszhl@users.noreply.github.com>
2024-09-26 18:07:52 +0800

    Add files via upload

---
+++ test-.py
@@ -0,0 +1,30 @@
+import contextlib
+from copy import copy
+from pathlib import Path
+
+import cv2
+import numpy as np
+import pytest
+import torch
+from PIL import Image
+from torchvision.transforms import ToTensor
+
+from ultralytics import RTDETR, YOLO
+from ultralytics.cfg import TASK2DATA
+from ultralytics.data.build import load_inference_source
+from ultralytics.utils import (ASSETS, DEFAULT_CFG, DEFAULT_CFG_PATH, LINUX, MACOS, ONLINE, ROOT, WEIGHTS_DIR, WINDOWS,
+                               checks, is_dir_writeable)
+from ultralytics.utils.downloads import download
+from ultralytics.utils.torch_utils import TORCH_1_9
+
+MODEL = WEIGHTS_DIR / 'path with spaces' / 'yolov8s.pt'  # test spaces in path
+CFG = 'models/dysample.yaml'
+SOURCE = ASSETS / 'bus.jpg'
+TMP = (ROOT / '../tests/tmp').resolve()  # temp directory for test files
+IS_TMP_WRITEABLE = is_dir_writeable(TMP)
+
+
+def test_model_forward():
+    """Test the forward pass of the YOLO model."""
+    model = YOLO(CFG)
+    model(SOURCE)  # also test no source and augment
